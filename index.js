f0628f5435f3ed1f54994406a92904e6243bed47
nstolk <nick_stolk9@hotmail.com>
2020-11-12 18:06:27 +0100

    Bump ytdl-core to v4.0.3

--- index.js
+++ index.js
@@ -46,7 +46,10 @@ async function execute(message, serverQueue) {
     const args = message.content.split(' ');
 
     const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+    if (!voiceChannel) {
+        return message.channel.send('You need to be in a voice channel to play music!');
+    }
+    
     const permissions = voiceChannel.permissionsFor(message.client.user);
     if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
         return message.channel.send('I need the permissions to join and speak in your voice channel!');
